6fa1979f8e0d31d71fb148c3d54db48504390257
Nathaly Gomes <nathaly.gomes@liferay.com>
2024-10-22 10:44:49 -0300

    LPD-37074 Create test to assert functioning of data provider on virtual instance with include and override property

--- modules/test/playwright/tests/dynamic-data-mapping-form-web/formSubmission.spec.ts
+++ modules/test/playwright/tests/dynamic-data-mapping-form-web/formSubmission.spec.ts
@@ -8,13 +8,25 @@ import path from 'path';
 
 import {formsPagesTest} from '../../fixtures/formsPagesTest';
 import {loginTest} from '../../fixtures/loginTest';
+import {virtualInstancesPagesTest} from '../../fixtures/virtualInstancesPagesTest';
+import {FormBuilderPage} from '../../pages/dynamic-data-mapping-form-web/FormBuilderPage';
+import {FormsPage} from '../../pages/dynamic-data-mapping-form-web/FormsPage';
+import performLogin from '../../utils/performLogin';
 import {deleteItems} from './utils/deleteItems';
 
-export const test = mergeTests(loginTest(), formsPagesTest);
+export const test = mergeTests(
+	loginTest(),
+	formsPagesTest,
+	virtualInstancesPagesTest
+);
 
 let hasDataProvider: boolean = false;
 
-test.afterEach(async ({formsPage, page}) => {
+const DEFAULT_VIRTUAL_INSTANCE_NAME = 'www.able.com';
+
+const deleteAfterTestVirtualInstances = new Set<string>();
+
+test.afterEach(async ({formsPage, page, virtualInstancesPage}) => {
 	await formsPage.goTo();
 
 	await deleteItems(formsPage, page);
@@ -28,9 +40,79 @@ test.afterEach(async ({formsPage, page}) => {
 
 		hasDataProvider = false;
 	}
+
+	for (const virtualInstanceName of deleteAfterTestVirtualInstances) {
+
+		await virtualInstancesPage.deleteVirtualInstance(virtualInstanceName);
+
+		deleteAfterTestVirtualInstances.delete(virtualInstanceName);
+	}
 });
 
 test.describe('Manage forms through submission page', () => {
+	test('assert that data provider works on virtual instance', async ({
+		browser,
+		virtualInstancesPage,
+	}) => {
+		await virtualInstancesPage.addNewVirtualInstance(
+			DEFAULT_VIRTUAL_INSTANCE_NAME
+		);
+
+		deleteAfterTestVirtualInstances.add(DEFAULT_VIRTUAL_INSTANCE_NAME);
+
+		const virtualInstancePage = await browser.newPage({
+			baseURL: `http://${DEFAULT_VIRTUAL_INSTANCE_NAME}:8080`,
+		});
+
+		await performLogin(
+			virtualInstancePage,
+			'test',
+			'?p_p_id=com_liferay_login_web_portlet_LoginPortlet&' +
+				'p_p_state=maximized',
+			`@${DEFAULT_VIRTUAL_INSTANCE_NAME}.com`
+		);
+
+		const virtualInstanceFormsPage = new FormsPage(virtualInstancePage);
+
+		await virtualInstanceFormsPage.goTo();
+
+		hasDataProvider = true;
+
+		await virtualInstanceFormsPage.importForm(
+			path.join(
+				__dirname,
+				'dependencies',
+				'form-with-data-provider.portlet.lar'
+			)
+		);
+
+		await virtualInstanceFormsPage.openForm('Form with data provider');
+
+		const pagePromise = virtualInstancePage.waitForEvent('popup');
+
+		const virtualInstanceFormBuilderPage = new FormBuilderPage(virtualInstancePage);
+
+		await virtualInstanceFormBuilderPage.openFormSubmission();
+
+		const virtualInstanceFormSubmissionPage = await pagePromise;
+
+		await virtualInstanceFormSubmissionPage.getByRole('button', {name: 'Submit'}).click();
+
+		await expect(
+			virtualInstanceFormSubmissionPage.getByText(
+				'Your information was successfully received. Thank you for filling out the form.'
+			)
+		).toBeVisible();
+
+		await virtualInstanceFormSubmissionPage.close();
+
+		await virtualInstanceFormBuilderPage.entriesTab.click();
+
+		await expect(virtualInstancePage.getByText('5379475')).toBeVisible();
+
+		await virtualInstancePage.close();
+	});
+
 	test('can submit manual entry while using data provider autofill rule', async ({
 		context,
 		formBuilderPage,
