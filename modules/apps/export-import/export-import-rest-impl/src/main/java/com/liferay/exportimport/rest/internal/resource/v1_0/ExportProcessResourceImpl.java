901f3257cbf951d38f19c7a6281534f33b639362
Daniel Raposo <daniel.raposo@liferay.com>
2026-08-16 12:58:21 +0200

    LPD-102309 Share the Content-Disposition header value logic
    ServletResponseUtil and PortletResponseUtil each built this header inline, and the export download endpoint had grown a third copy inside a resource impl, where the method had to be widened to protected for the test to reach it. The shared version also declines to quote a name holding a quote or a backslash, which the other two would have let close the filename parameter early.
    
    (cherry picked from commit 4e54fd7a7df120b0b14f23bc8402b35ea04d7973)

--- modules/apps/export-import/export-import-rest-impl/src/main/java/com/liferay/exportimport/rest/internal/resource/v1_0/ExportProcessResourceImpl.java
+++ modules/apps/export-import/export-import-rest-impl/src/main/java/com/liferay/exportimport/rest/internal/resource/v1_0/ExportProcessResourceImpl.java
@@ -41,12 +41,12 @@ import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.service.PortletLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.servlet.ContentDispositionUtil;
 import com.liferay.portal.kernel.util.DateRange;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.URLCodec;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
@@ -54,6 +54,7 @@ import com.liferay.staging.StagingGroupHelper;
 
 import jakarta.ws.rs.BadRequestException;
 import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.Response;
 
 import java.io.Serializable;
@@ -153,8 +154,9 @@ public class ExportProcessResourceImpl extends BaseExportProcessResourceImpl {
 		return Response.ok(
 			fileEntry.getContentStream()
 		).header(
-			"Content-Disposition",
-			getContentDispositionHeaderValue(fileEntry.getTitle())
+			HttpHeaders.CONTENT_DISPOSITION,
+			ContentDispositionUtil.getContentDispositionHeaderValue(
+				fileEntry.getTitle())
 		).build();
 	}
 
@@ -314,11 +316,6 @@ public class ExportProcessResourceImpl extends BaseExportProcessResourceImpl {
 		return _postPortletExportProcess(
 			exportProcessRequest, _getSiteGroup(siteExternalReferenceCode),
 			GetterUtil.getLong(plid), portletId);
-	}
-
-	protected static String getContentDispositionHeaderValue(String fileName) {
-		return "attachment; filename*=UTF-8''" +
-			URLCodec.encodeURL(fileName, true);
 	}
 
 	private Group _getAssetLibraryGroup(String externalReferenceCode) {
