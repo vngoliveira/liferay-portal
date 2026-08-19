2777815475e5496e22c9dbe33336c62a351c4e67
Brian Chan <brian.chan@liferay.com>
2026-08-19 18:49:07 +0800

    LPD-101901 SF

--- modules/apps/depot/depot-test/src/testIntegration/java/com/liferay/depot/internal/security/permission/wrapper/test/DepotPermissionCheckerWrapperTest.java
+++ modules/apps/depot/depot-test/src/testIntegration/java/com/liferay/depot/internal/security/permission/wrapper/test/DepotPermissionCheckerWrapperTest.java
@@ -299,12 +299,6 @@ public class DepotPermissionCheckerWrapperTest {
 						PermissionChecker permissionChecker =
 							_permissionCheckerFactory.create(user);
 
-						Assert.assertTrue(
-							permissionChecker.hasPermission(
-								null, User.class.getName(),
-								assetLibraryMemberUser.getUserId(),
-								ActionKeys.VIEW));
-
 						Assert.assertFalse(
 							permissionChecker.hasPermission(
 								null, User.class.getName(), _user.getUserId(),
@@ -317,6 +311,11 @@ public class DepotPermissionCheckerWrapperTest {
 							permissionChecker.hasPermission(
 								null, User.class.getName(), _user.getUserId(),
 								ActionKeys.UPDATE));
+						Assert.assertTrue(
+							permissionChecker.hasPermission(
+								null, User.class.getName(),
+								assetLibraryMemberUser.getUserId(),
+								ActionKeys.VIEW));
 					}));
 	}
 
