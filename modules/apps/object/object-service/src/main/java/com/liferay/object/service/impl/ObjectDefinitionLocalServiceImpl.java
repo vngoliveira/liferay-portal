0311f63e29ad1a74cb89e4b609f598d062438c33
Pedro Leite <pedro.leite@liferay.com>
2026-08-27 16:06:03 -0300

    LPD-103892 Undeploy object definitions in the definition's company

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -1186,19 +1186,24 @@ public class ObjectDefinitionLocalServiceImpl
 			return;
 		}
 
-		_undeploy(
-			_objectDefinitionDeployer,
-			_objectDefinitionDeployerServiceRegistrationsMap, objectDefinition);
+		try (SafeCloseable safeCloseable = CompanyThreadLocal.lock(
+				objectDefinition.getCompanyId())) {
 
-		for (Map.Entry
-				<ObjectDefinitionDeployer,
-				 Map<String, List<ServiceRegistration<?>>>> entry :
-					_activeServiceRegistrationsMaps.entrySet()) {
+			_undeploy(
+				_objectDefinitionDeployer,
+				_objectDefinitionDeployerServiceRegistrationsMap,
+				objectDefinition);
 
-			_undeploy(entry.getKey(), entry.getValue(), objectDefinition);
+			for (Map.Entry
+					<ObjectDefinitionDeployer,
+					 Map<String, List<ServiceRegistration<?>>>> entry :
+						_activeServiceRegistrationsMaps.entrySet()) {
+
+				_undeploy(entry.getKey(), entry.getValue(), objectDefinition);
+			}
+
+			_invalidatePortalCache(objectDefinition);
 		}
-
-		_invalidatePortalCache(objectDefinition);
 	}
 
 	@Indexable(type = IndexableType.REINDEX)
